b84452c1f05907288bc15f8c614f09b21508db8b
adriankr38 <adrian58ago@gmail.com>
2023-03-01 12:53:22 +0000

    clean(java):remove comments/unused funcs

--- assignments/week3/cars/Car.java
+++ assignments/week3/cars/Car.java
@@ -1,8 +1,6 @@
 import java.lang.reflect.Field;
 import java.lang.IllegalAccessException;
 import java.util.Optional;
-import java.util.Map;
-import java.util.HashMap;
 import java.util.Arrays;
 
 public abstract class Car{
@@ -40,78 +38,6 @@ public abstract class Car{
     }
     return outputString;
   }
-
-
-
-
-  // public String getFieldsAsString() throws IllegalAccessException{
-  //   String outputString = "";
-  //   Class<? extends Car>instanceOfClass = this.getClass();
-  //   Field[]superClassFields = instanceOfClass.getSuperclass().getDeclaredFields();
-  //   Field[]ownClassFields= instanceOfClass.getDeclaredFields();
-  //   Field[]isntanceFields = Arrays.copyOf(superClassFields,superClassFields.length);
-  //   System.arraycopy(ownClassFields,0,instanceFields,superClassFields.length,ownClassFields.length);
-  //
-  //   for (Field f:instanceFields){
-  //     try{
-  //
-  //       f.setAccessible(false);
-  //       String fieldName = f.getName();
-  //       Optional<Object> fieldObject = Optional.ofNullable(f.get(this));
-  //       if(fieldObject.isPresent()){
-  //         outputString += fieldName + ":" + fieldObject.get().toString() + ";";
-  //       }
-  //     }catch(IllegalAccessException e){
-  //       e.printStackTrace();
-  //     }
-  //   }
-  //   return outputString;
-  // }
-
-
-  // public String getFieldsAsString() throws IllegalAccessException{
-  //   String outputString = "";
-  //   Class<? extends Car> instanceOfCar = this.getClass();
-  //   Field[]superClassFields = instanceOfCar.getSuperclass().getDeclaredFields();
-  //   Field[]ownClassFields= instanceOfCar.getDeclaredFields();
-  //   Field[]instanceFields = Arrays.copyOf(superClassFields,superClassFields.length);
-  //   System.arraycopy(ownClassFields,0,instanceFields,superClassFields.length,ownClassFields.length);
-  //   for (Field f:instanceFields){
-  //     try{
-  //       f.setAccessible(false);
-  //       String fieldName = f.getName();
-  //       Optional<Object>  fieldObject = Optional.ofNullable(f.get(this));
-  //     if (fieldObject.isPresent()){
-  //       outputString += fieldName + ":" + fieldObject.get().toString() + ";";
-  //       
-  //     }
-  //     }catch(IllegalAccessException e){
-  //       e.printStackTrace();
-  //     }
-  //   }
-  //   return outputString;
-  // }
-
-  public Map<String,Object> toMap() throws IllegalAccessException{
-    Class<? extends Car> instanceClass = this.getClass();
-    Map<String,Object> outputMap = new HashMap<>();
-
-    Field[]superClassFields = instanceClass.getSuperclass().getDeclaredFields();
-    Field[]ownClassFields= instanceClass.getDeclaredFields();
-    Field[]instanceFields = Arrays.copyOf(superClassFields,superClassFields.length);
-    System.arraycopy(ownClassFields,0,instanceFields,superClassFields.length,ownClassFields.length);
-
-    for (Field f:instanceFields){
-      try{
-        f.setAccessible(false);
-        outputMap.put(f.getName(),f.get(this));
-      }catch(IllegalAccessException e){
-        e.printStackTrace();
-      }
-    }
-    return outputMap;
-  }
-
 
 
   public int getVinNumber() {
