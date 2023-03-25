3c95caff919bbc0a420f3a44bd1376102cc80dae
adriankr38 <adrian58ago@gmail.com>
2023-03-25 13:24:52 +0100

    fix(java):Account

--- third_week/src/Account.java
+++ third_week/src/Account.java
@@ -1,41 +1,161 @@
 public class Account{
   protected String name;
   protected String address;
-  protected long accountNumber;
+  protected long accopublic void setAccountNumber(long accountNumber) {
+	this.accountNumber = accountNumber;
+}
+public void setBalance(int balance) {
+	this.balance = balance;
+}
+unt
+
+public String getName() {
+	return name;
+}
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+public String getAddress() {
+	return address;
+}
+
+public void setAddress(String address) {
+	this.address = address;
+}
+
+public double getBalance() {
+	return balance;
+}
+
+public void setBalance(double balance) {
+	this.balance = balance;
+}
+
+public String getAccountNumber() {
+	return accountNumber;
+}
+
+public void setAccountNumber(String accountNumber) {
+	this.accountNumber = accountNumber;
+}
+
+public Hold getHold() {
+	return hold;
+}
+
+public void setHold(Hold hold) {
+	this.hold = hold;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+public String getAddress() {
+	return address;
+}
+
+public void setAddress(String address) {
+	this.address = address;
+}
+
+public double getBalance() {
+	return balance;
+}
+
+public void setBalance(double balance) {
+	this.balance = balance;
+}
+
+public String getAccountNumber() {
+	return accountNumber;
+}
+
+public void setAccountNumber(String accountNumber) {
+	this.accountNumber = accountNumber;
+}
+
+public Hold getHold() {
+	return hold;
+}
+
+public void setHold(Hold hold) {
+	this.hold = hold;
+}Number;
   protected int balance;
 
-  Account(String name, String address, long accountNumber, int balance){
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getAddress() {
+	return address;
+}
+public void setAddress(String address) {
+	this.address = address;
+}
+public double getBalance() {
+	return balance;
+}
+public void setBalance(double balance) {
+	this.balance = balance;
+}
+public String getAccountNumber() {
+	return accountNumber;
+}
+public void setAccountNumber(String accountNumber) {
+	this.accountNumber = accountNumber;
+}
+public Hold getHold() {
+	return hold;
+}
+public void setHold(Hold hold) {
+	this.hold = hold;
+}
+  Account(String name, String address, long accountNumber, int balance, Hold hold){
     setName(name);
     setAddress(address);
     setAccountNumber(accountNumber);
     setBalance(balance);
+    setHold(hold);
   }
 
-  protected String getName() {
+  public String getName() {
     return name;
   }
-  protected void setName(String name) {
+  public void setName(String name) {
     this.name = name;
   }
-  protected String getAddress() {
+  public String getAddress() {
     return address;
   }
-  protected void setAddress(String address) {
+  public void setAddress(String address) {
     this.address = address;
   }
-  protected long getAccountNumber() {
-    return accountNumber;
-  }
-  protected void setAccountNumber(long accountNumber) {
-    this.accountNumber = accountNumber;
-  }
-  protected int getBalance() {
+  public double getBalance() {
     return balance;
   }
-  protected void setBalance(int balance) {
+  public void setBalance(double balance) {
     this.balance = balance;
   }
-
-
-  
+  public String getAccountNumber() {
+    return accountNumber;
+  }
+  public void setAccountNumber(String accountNumber) {
+    this.accountNumber = accountNumber;
+  }
+  public Hold getHold() {
+    return hold;
+  }
+  public void setHold(Hold hold) {
+    this.hold = hold;
+  }
 }
