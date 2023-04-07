2308e1bbcaa2b46e1e8b3359ffac5955c4dd6ad2
adriankr38 <adrian58ago@gmail.com>
2023-04-07 11:52:25 +0200

    add(java): cfu setup

---
+++ eigth_week/src/test/java/com/ironhack/eigth_week/EigthWeekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhack.eigth_week;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EigthWeekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
