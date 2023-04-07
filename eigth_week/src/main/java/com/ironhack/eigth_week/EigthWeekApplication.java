ad63240f3467111e0e64710a603eb850003794ce
adriankr38 <adrian58ago@gmail.com>
2023-04-07 11:51:54 +0200

    add(java): cfu setup

---
+++ eigth_week/src/main/java/com/ironhack/eigth_week/EigthWeekApplication.java
@@ -0,0 +1,13 @@
+package com.ironhack.eigth_week;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EigthWeekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EigthWeekApplication.class, args);
+	}
+
+}
